8b3716fac79602e3ec6f23321f00e16e2fd2e000
Kamnish Chandra <kamnish@Kamnishs-MacBook-Air.local>
2023-07-15 01:19:10 +0530

    New hover effect...Horizontal overflow fixed. for mobile view contact me section text alignment, image fade

--- script.js
+++ script.js
@@ -39,3 +39,16 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     }
   });
+// --------------------------Cool hover animation --------------------------------
+const enhance= id =>{
+  const element = document.getElementById(id);
+  text=element.innerText.split("");
+  element.innerText="";
+  text.forEach(letter=>{
+      const span = document.createElement("span");
+      span.className="letter";
+      span.innerText=letter;
+      element.appendChild(span);
+});
+}
+enhance("pro");
